ef7e961ca2d9407eff84b9efed6dcc52c1eb5ca3
Давид Уланович <david20222009@gmail.com>
2024-04-05 20:19:36 +0300

    modified main.js

--- main.js
+++ main.js
@@ -3,30 +3,35 @@ var clicks = 0;
 var getRandomNumber = function (size) {
     return Math.floor(Math.random() * size);
 };
+//Создаем переменные
 var width = 400;
 var height = 400;
-
+//случайная позиция клада
 var target = {
     x: getRandomNumber(width),
     y: getRandomNumber(height)
 };
-
+//Добавляем элементу img обработчик клика
 $("#map").click(function (event) {
        clicks++;
+       //Получаем расстояние от места клика до клада
        var distance = getDistance(event, target);
+       //Преобразуем расстояние в подсказку
        var distanceHint = getDistanceHint(distance);
+       //Записываем в элемент #distance новую подсказку
        $("#distance").text(distanceHint);
+       //Если клик был достаточно близко позровляем с победой
        if (distance < 8) {
         alert("Клад найден! Сделано кликов: " + clicks);
 }
 });
-
+// Вычислить расстояние от клика (event) до клада (taarget)
 var getDistance = function (event, target) {
     var diffX = event.offsetX - target.x;
     var diffY = event.offsetY - target.y;
     return Math.sqrt((diffX * diffX) + (diffY * diffY));
 };
-
+// Получит для расстояния строку подсказки
 var getDistanceHint = function (distance) {
     if (distance < 10) {
       return "Обожжешься!";
